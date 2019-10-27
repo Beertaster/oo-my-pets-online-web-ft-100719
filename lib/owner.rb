2ab747dc69229ebb89aa4bff8169bc3763fe1289
William Stevens <wstevens.hornet@gmail.com>
2019-10-27 20:45:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -11,24 +11,34 @@ class Owner
     @pets = {:cat => [], :dog => []}
   end
   
-  def buy_dog(name_of_dog)
-    @pets[:dogs] << Dog.new(name_of_dog)
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
   end
 
-  def buy_cat(name_of_cat)
-    @pets[:cats] << Cat.new(name_of_cat)
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
   end
   
+  
+  
   def walk_dogs
-    @pets.collect do |species, instances|
-      if species == :dogs
-        instances.each do |dog|
-          dog.mood = "happy"
+    @pets.collect do |species, instance|
+      if species == :dog
+        instance.each do |pooch|
+          pooch.mood = "happy"
         end
       end
     end
   end
   
-  
+  def feed_cats
+    @pets.collect do |species, instance|
+      if species == :cat
+        instance.each do |kitty|
+          kitty.mood = "happy"
+        end
+      end
+    end
+  end
   
 end
