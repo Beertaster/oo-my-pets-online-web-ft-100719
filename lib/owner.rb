840a9008b57528efdaf6c2436048190222ee0b1e
William Stevens <wstevens.hornet@gmail.com>
2019-10-27 20:35:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -11,6 +11,12 @@ class Owner
     @pets = {:cat => [], :dog => []}
   end
   
-  
+  def buy_dog(name_of_dog)
+    @pets[:dogs] << Dog.new(name_of_dog)
+  end
+
+  def buy_cat(name_of_cat)
+    @pets[:cats] << Cat.new(name_of_cat)
+  end
   
 end
